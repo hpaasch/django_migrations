ff1dbb2b5737082a710ddb3e7c2a626c388aa520
hpaasch <hepaasch@gmail.com>
2016-06-06 14:54:25 -0400

    Homework complete.

--- hep_app/admin.py
+++ hep_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from hep_app.models import SkiStats
+
+admin.site.register(SkiStats)
--- hep_app/models.py
+++ hep_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SkiStats(models.Model):
+    rank = models.IntegerField()
+    athlete = models.CharField(max_length=50)
+    age = models.IntegerField()
+    team = models.CharField(max_length=50)
+    noc = models.CharField(max_length=3)
+    medal = models.CharField(max_length=10)
+    t_time = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.athlete
